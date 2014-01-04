9a5d6e599f323bc0adf0f430f255f555104273a4
Kris Kowal <kris.kowal@cixar.com>
2014-01-04 12:28:51 -0800

    Slighly improve verification

--- verify.js
+++ verify.js
@@ -4,3 +4,12 @@ if (WeakMap === global.WeakMap) {
 } else {
     console.log("shim");
 }
+var wm1 = new WeakMap();
+var wm2 = new WeakMap();
+var o = {};
+wm1.set(o, 10);
+wm2.set(o, 20);
+if (wm1.get(o) !== 10) {
+    console.log("error");
+}
+
